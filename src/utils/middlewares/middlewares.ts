8f6d01415c7faad11e24da3022bc79facc93641a
Lory-Stan TANASI <stantanasi@gmail.com>
2021-12-29 16:41:51 +0100

    Create isLogin middleware

--- src/utils/middlewares/middlewares.ts
+++ src/utils/middlewares/middlewares.ts
@@ -2,6 +2,30 @@ import { NextFunction, Request, Response } from "express";
 import { UserModel } from "../../models/user.model";
 import { PermissionDenied } from "../json-api/json-api.error";
 
+export const isLogin = () => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let bearerToken = req.headers.authorization;
+    if (bearerToken?.startsWith('Bearer ')) {
+      bearerToken = bearerToken.substring(7);
+    }
+
+    if (!bearerToken) {
+      throw new PermissionDenied();
+    }
+
+    const user = await UserModel.findOne({
+      uid: bearerToken,
+    });
+    if (!user) {
+      throw new PermissionDenied();
+    }
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
 export const isAdmin = () => async (req: Request, res: Response, next: NextFunction) => {
   try {
     let bearerToken = req.headers.authorization;

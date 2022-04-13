aa169936debbbfbd5044add9922aece10fa71e6f
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-13 14:13:05 +0200

    refactor(app): remove unused code

--- src/app.ts
+++ src/app.ts
@@ -36,16 +36,6 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/favicon.ico', (_req, res) => res.status(204).send());
 
 app.use((req, res, next) => {
-  if (req.method === 'POST') {
-    if (req.body?.REQUEST_METHOD) {
-      req.method = req.body.REQUEST_METHOD;
-      req.body = req.body.data;
-    }
-  }
-  next();
-});
-
-app.use((req, res, next) => {
   JsonApiSerializer.initialize({
     baseUrl: `${req.protocol}://${req.get('host')}`,
   });

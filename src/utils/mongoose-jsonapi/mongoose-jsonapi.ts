25e58f0daa3e2bcf8d633968f3a8ba3c6152dff6
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-27 12:39:22 +0200

    feat(mongoose-jsonapi): create JsonApiError class

--- src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
+++ src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
@@ -495,10 +495,10 @@ export interface JsonApiBody {
     next?: string;
     last?: string;
   };
-  errors?: JsonApiError[];
+  errors?: IJsonApiError[];
 }
 
-export interface JsonApiError {
+export interface IJsonApiError {
   id?: string;
   links?: {
     about?: string;
@@ -509,7 +509,7 @@ export interface JsonApiError {
   detail?: string;
   source?: {
     pointer?: string;
-    parameter: string;
+    parameter?: string;
   };
   meta?: any;
 }
@@ -540,4 +540,44 @@ export interface JsonApiResource {
     }
   };
   meta?: any;
+}
+
+
+export class JsonApiError extends Error implements IJsonApiError {
+
+  id?: string;
+  links?: {
+    about?: string;
+  };
+  status?: string;
+  code?: string;
+  title?: string;
+  detail?: string;
+  source?: {
+    pointer?: string;
+    parameter?: string;
+  };
+  meta?: any;
+
+  constructor(public obj: IJsonApiError) {
+    super();
+    Object.assign(this, obj);
+  }
+
+  static from(err: Error): JsonApiError {
+    return new JsonApiError({
+      status: '500',
+      title: err.name,
+      detail: err.message,
+      meta: err.stack,
+    });
+  }
+
+  toJSON(): JsonApiBody {
+    return {
+      errors: [
+        this.obj,
+      ],
+    };
+  }
 }

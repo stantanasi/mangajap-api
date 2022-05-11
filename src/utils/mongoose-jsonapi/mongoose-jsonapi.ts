7f3929fee9938d4b93bc1e3ea309070e640db5ee
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-11 18:04:44 +0200

    refactor(mongoose-jsonapi): reformat code

--- src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
+++ src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
@@ -1,6 +1,7 @@
 import {
   Document,
   Error as MongooseError,
+  FilterQuery,
   HydratedDocument,
   Model,
   PopulateOptions,
@@ -13,7 +14,12 @@ import UrlQuery from '../url-query/url-query';
 
 export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>>(
   _schema: Schema<DocType, M>,
-  options?: JsonApiPluginOptions,
+  options: {
+    type: string;
+    filter?: {
+      [field: string]: (value: string) => FilterQuery<DocType>;
+    };
+  },
 ): void {
   const schema = _schema as Schema<DocType, M, JsonApiInstanceMethods, JsonApiQueryHelper>;
 
@@ -68,7 +74,7 @@ export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>
               return {
                 $or: values.split(',')
                   .map((value: string) => {
-                    if (options?.filter?.[field]) {
+                    if (options.filter?.[field]) {
                       return options.filter[field](value);
                     } else {
                       return { [field]: value };
@@ -188,10 +194,10 @@ export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>
               return {
                 $or: values.split(',')
                   .map((value: string) => {
-                    if (options?.filter?.[field]) {
+                    if (options.filter?.[field]) {
                       return options.filter[field](value);
                     } else {
-                      return { [field]: value };
+                      return { [field]: value } as any;
                     }
                   })
               };
@@ -335,7 +341,7 @@ export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>
 
     const obj: any = this.toObject();
 
-    const type = options?.type;
+    const type = options.type;
     const id = this._id?.toString();
 
     if (!type) {
@@ -446,13 +452,6 @@ export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>
     });
 
     return Object.assign(this, ...sources);
-  };
-}
-
-export interface JsonApiPluginOptions {
-  type: string;
-  filter?: {
-    [field: string]: (value: string) => any;
   };
 }
 

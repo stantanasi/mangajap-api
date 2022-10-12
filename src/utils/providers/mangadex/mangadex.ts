f9a43915cde0c1073bf46f354681ca2dd2571f2c
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-12 19:49:16 +0200

    feat(mangadex): implement find manga by id

--- src/utils/providers/mangadex/mangadex.ts
+++ src/utils/providers/mangadex/mangadex.ts
@@ -1,3 +1,85 @@
+import axios from 'axios';
+import Chapter from '../../../models/chapter.model';
+import Manga from '../../../models/manga.model';
+import Volume from '../../../models/volume.model';
+
+export default class MangaDex {
+
+  public static async findMangaById(id: string) {
+    const manga = await axios.get<MangaDexManga>(`https://api.mangadex.org/manga/${id}`)
+      .then((res) => res.data)
+      .then((response) => {
+        if (response.result === 'error') {
+          return null;
+        } else {
+          return new Manga({
+            title: response.data.attributes.title.en,
+            titles: response.data.attributes.altTitles.reduce((acc, cur) => {
+              return Object.assign(acc, cur);
+            }, {}),
+
+            volumes: [],
+            chapters: [],
+          });
+        }
+      });
+
+    if (manga === null) return manga;
+
+    await axios.get<MangaDexVolumes>(`https://api.mangadex.org/manga/${id}/aggregate`)
+      .then((res) => res.data)
+      .then((data) => {
+        Object.values(data.volumes).forEach((volume) => {
+          const chapters = Object.values(volume.chapters)
+            .filter((chapter) => +chapter.chapter && Number.isInteger(+chapter.chapter))
+            .map((chapter) => {
+              return new Chapter({
+                number: +chapter.chapter,
+              });
+            });
+
+          if (+volume.volume && Number.isInteger(+volume.volume)) {
+            manga.volumes = manga.volumes?.concat(new Volume({
+              number: +volume.volume,
+
+              chapters: chapters,
+            }));
+          }
+
+          manga.chapters = manga.chapters?.concat(chapters);
+        });
+      });
+
+    for (let i = 0; i < (manga.volumes?.length ?? 0) / 100; i++) {
+      await axios.get<MangaDexCovers>(`https://api.mangadex.org/cover?manga[]=${id}&limit=100&offset=${i * 100}&order[volume]=asc`)
+        .then((res) => res.data)
+        .then((covers) => covers.data.filter((cover) => cover.attributes.locale === 'ja'))
+        .then((covers) => covers.filter((cover) => +cover.attributes.volume && Number.isInteger(+cover.attributes.volume)))
+        .then((covers) => {
+          covers.forEach((cover) => {
+            let volume = manga.volumes?.find((volume) => volume.number === +cover.attributes.volume)
+
+            if (volume) {
+              volume.coverImage = cover ?
+                `https://uploads.mangadex.org/covers/${id}/${cover.attributes.fileName}` :
+                null;
+            } else {
+              volume = new Volume({
+                number: +cover.attributes.volume,
+                coverImage: cover ?
+                  `https://uploads.mangadex.org/covers/${id}/${cover.attributes.fileName}` :
+                  null,
+              });
+              manga.volumes?.push(volume);
+            }
+          });
+        })
+    }
+
+    return manga;
+  }
+}
+
 export interface MangaDexManga {
   result: 'ok' | 'error'
   response: 'entity'

eb38216e9ef00e63acd132afc0e3dc23d43929c9
Lory-Stan TANASI <stantanasi@gmail.com>
2023-12-29 21:04:25 +0100

    feat(mongoose-jsonapi): throw JSON:API error on resource not found

--- src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
+++ src/utils/mongoose-jsonapi/mongoose-jsonapi.ts
@@ -247,7 +247,9 @@ export default function MongooseJsonApi<DocType, M extends JsonApiModel<DocType>
   schema.query.toJsonApi = function (opts) {
     // Throw an error if no document has been found
     if ((this as any).op === 'findOne') {
-      this.orFail();
+      this.orFail(() => {
+        throw new JsonApiError.ResourceNotFoundError(this.getFilter()._id);
+      });
     }
 
     return this.transform((doc) => {

0d730f8e31736a61b4354630aab947cf852629c4
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-02 16:17:22 +0100

    feat(mongoose-multi-language): implement update path transform

--- src/utils/mongoose-multi-language/mongoose-multi-language.ts
+++ src/utils/mongoose-multi-language/mongoose-multi-language.ts
@@ -22,4 +22,34 @@ export default function MongooseMultiLanguage<DocType extends { _id: any }, M ex
   },
 ) {
   const schema = _schema as Schema<DocType, M, MultiLanguageInstanceMethods, MultiLanguageQueryHelper, {}, MultiLanguageModel<DocType>>;
+
+  options.fields.forEach((path) => {
+    const transform = schema.path(path).options.transform;
+
+    schema.path(path).options.transform = function (this, value: any) {
+      value = transform ? transform.call(this, value) : value;
+
+      const language = this._language;
+      if (!language) {
+        return value;
+      }
+
+      if (typeof value === "object") {
+        let translation;
+        if (value instanceof Map) {
+          translation = value.get(language) ??
+            [...value.entries()].find(([key]) => key.startsWith(language.split("-")[0]))?.[1] ??
+            value.get(DEFAULT_LANGUAGE);
+        } else {
+          translation = value[language] ??
+            Object.entries(value).find(([key]) => key.startsWith(language.split("-")[0]))?.[1] ??
+            value[DEFAULT_LANGUAGE];
+        }
+
+        return translation ?? null;
+      }
+
+      return value;
+    };
+  });
 }

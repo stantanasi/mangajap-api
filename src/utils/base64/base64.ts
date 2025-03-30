d3816e168336363098f29d7b9321d0d617079cf5
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-30 14:04:36 +0200

    feat: create Base64 utils class to get base64 from image URL

---
+++ src/utils/base64/base64.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default abstract class Base64 {
+
+  static async fromImageURL(url: string): Promise<string> {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    return Buffer.from(response.data, 'binary').toString('base64');
+  }
+}

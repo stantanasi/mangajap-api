8db44d0e5ccdcb6812f4714653227ce3173a4082
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-04 18:48:17 +0200

    feat(app): temporarily disable scheduled anime/manga sync job

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,6 @@ import themeRoutes from "./routes/theme.routes";
 import userRoutes from "./routes/user.routes";
 import volumeEntryRoutes from "./routes/volume-entry.routes";
 import volumeRoutes from "./routes/volume.routes";
-import AnimeService from "./services/anime.service";
-import MangaService from "./services/manga.service";
 
 const app = express();
 
@@ -172,6 +170,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 export const api = functions.https.onRequest(app);
 
 export const sync = onSchedule('30 2 * * *', async (event) => {
-  await AnimeService.sync();
-  await MangaService.sync();
+  // await AnimeService.sync();
+  // await MangaService.sync();
 });

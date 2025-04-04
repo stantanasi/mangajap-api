866770bfe0e668f23c322ba84fdb5a41d846e9da
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-04 12:15:50 +0200

    feat(manga.service): create mangadex service to sync manga

--- src/services/manga.service.ts
+++ src/services/manga.service.ts
@@ -1,5 +1,13 @@
+abstract class MangaDexService {
+
+  static async sync() {
+  }
+}
+
+
 export default abstract class MangaService {
 
   static async sync() {
+    await MangaDexService.sync();
   }
 }
